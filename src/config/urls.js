a69417d7f539a27a1bafa117b96b59106c2bc982
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-02 02:25:36 -0300

    urls

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,5 +1,5 @@
 const URL_BACKEND_LOCAL = 'http://localhost:8080';
-const URL_BACKEND_REMOTE = 'https://almerindoflix.herokuapp.com';
+const URL_BACKEND_REMOTE = 'https://guanabaraflix.herokuapp.com';
 const URL_CATEGORIES = '/categorias';
 const URL_VIDEOS = '/videos';
 
